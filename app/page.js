601bcb3a375c8118e865090c9ec96617fc9a0ba9
Digital Esposure Online Services <131973180+Deos2023@users.noreply.github.com>
2025-07-21 15:39:48 +0530

    Update

--- app/page.js
+++ app/page.js
@@ -49,7 +49,7 @@ export default function Home() {
           playsInline
           className="absolute top-0 left-0 w-full h-full object-cover"
         >
-          <source src="/vid1.mp4" type="video/mp4" />
+          <source src="/vid2.mp4" type="video/mp4" />
           Your browser does not support the video tag.
         </video>
 
